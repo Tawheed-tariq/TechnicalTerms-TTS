80e9c8b34d81fccc85cdf796330cf4a9bea1cd39
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-10-27 21:52:45 +0530

    idk

---
+++ eval.py
@@ -0,0 +1,21 @@
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from datasets import load_dataset
+import torch
+import soundfile as sf
+
+commit_hash = "1c832de03aeca79cebf3c0242260159cee863a44"
+model = SpeechT5ForTextToSpeech.from_pretrained("tawheed-tariq/speecht5_tts", revision=commit_hash)
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+text = "Text-to-speech, or TTS, transforms text input into spoken voice output using advanced neural networks."
+inputs = processor(text=text, return_tensors="pt")
+
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation", trust_remote_code=True)
+
+speaker_embeddings = embeddings_dataset[7308]["xvector"]
+speaker_embeddings = torch.tensor(speaker_embeddings).unsqueeze(0)
+
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+sf.write("output.wav", speech.numpy(), samplerate=16000)
